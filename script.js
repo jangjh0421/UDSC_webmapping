66e7535d4424cc38e03825e1318921b60bce82ef
Jaehyeong Jang <jaehyeong.jang@mail.utoronto.ca>
2024-05-08 21:30:36 -0400

    updated

--- script.js
+++ script.js
@@ -126,20 +126,6 @@ cho_el.id = 'marker16';
 //----------------------------------------
 
 map.on('load', () => {
-    
-    map.addLayer({
-        'id': 'route',
-        'type': 'line',
-        'source': 'route',
-        'layout': {
-            'line-join': 'round',
-            'line-cap': 'round'
-        },
-        'paint': {
-            'line-color': '#888',
-            'line-width': 4
-        }
-    });
 
 //----------------------------------------    
 
